74a5959423fb2aa112f9d5256263841c65154f9a
unknown <samvthom16@gmail.com>
2019-08-02 13:21:20 +0530

    Add website links in the bio of author

--- single.php
+++ single.php
@@ -15,6 +15,24 @@
             <div class="orbit-author-meta">
               <h3><a href="<?php _e( do_shortcode( '[orbit_author_link]' ) );?>"><?php _e( do_shortcode( '[orbit_author]' ) );?></a></h3>
               <p><?php _e( get_user_meta( $post->post_author, 'description', true ) );?></p>
+              <?php
+
+                $user_links = array(
+                  'user_url'  => 'Website'
+                );
+
+                _e( "<ul class='list-inline'>" );
+                foreach ( $user_links as $key => $label ) {
+                  $link = get_the_author_meta( $key );
+                  if( $link ){
+                    _e( "<li><a href='".$link."'>" . $label . "</a></li>" );
+                  }
+                }
+                _e( "</ul>" );
+
+              ?>
+
+
             </div>
           </div>
         </div>
