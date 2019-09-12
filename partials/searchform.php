77e19c815ac4ead12043d6b887a9627a2c75d861
unknown <samvthom16@gmail.com>
2019-09-12 14:53:39 +0530

    Search template

--- partials/searchform.php
+++ partials/searchform.php
@@ -1,4 +1,4 @@
-<form class="inline-search-form" role="search" method="get" id="searchform" action="<?php bloginfo('url');?>">
+<form class="inline-search-form" role="search" method="get" action="<?php bloginfo('url');?>">
   <div class="input-group add-on">
     <input class="form-control" placeholder="Search for churches or articles" name="s" id="s" type="text" value="<?php _e( get_search_query() );?>" />
     <div class="input-group-btn">
